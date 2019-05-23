cbbe789ef799d8793034ae1f202057245b9d08a2
mucheng <mucheng@muchengdeiMac.local>
2019-05-23 14:58:26 +0800

    [5.22]:finish lcd test

--- main/main.js
+++ main/main.js
@@ -1,4 +1,28 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var lcd = [
+        ['._.', '|.|', '|_|'],
+        ['...', '..|', '..|'],
+        ['._.', '._|', '|_.'],
+        ['._.', '._|', '._|'],
+        ['...', '|_|', '..|'],
+        ['._.', '|_.', '._|'],
+        ['._.', '|_.', '|_|'],
+        ['._.', '..|', '..|'],
+        ['._.', '|_|', '|_|'],
+        ['._.', '|_|', '..|'],
+    ]
+    var numLength = num.length
+    var numArr = num.split('')
+    var res = ''
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < numLength; j++) {
+            if (j === numLength - 1) {
+                res = res + lcd[Number(numArr[j])][i]
+                res = res + '\n'
+            } else {
+                res = res + lcd[Number(numArr[j])][i] + ' '
+            }
+        }
+    }
+    return res;
 };
